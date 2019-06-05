a46e97712dd4f2cf39ef9d620b5b38bf39033541
Nayara Mattos <nayyaramattos@gmail.com>
2019-06-05 17:58:37 -0300

    Alterações Json

---
+++ head.php
@@ -0,0 +1,24 @@
+<?php
+include "funcoes.php";
+$usuario = logarUsuario("Nayara Mattos", 1);
+//$usuario = "";
+
+$jsonProdutos = file_get_contents("Produtos.json");
+
+$produtos = json_decode($jsonProdutos,true);
+
+$produtos = $produtos["Produtos"];
+
+addProduto("Curso Mobile Android", "Curso para criar um app",2300,"img/produto2.jpg");
+$categorias = ["Cursos", "Tutoriais", "Artigos", "Forum", "Códigos"];
+?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Loja Virtual</title>
+    <!-- -->
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
--- index.php
+++ index.php
@@ -1,158 +1,87 @@
-   <?php
-$usuario = ["logado" => true,"nome" => "Nayara","nivelAcesso" => 02];
-//$usuario ="";
 
-$produtos = [
-"produto1" => ["nome"=> "Curso Fullstack","descricao" => "Um curso de Programação incrivel" ,"preco" =>1200,"img" => "img/produto1.jpeg"],
-"produto2" => ["nome"=> "Curso Mobile Android","descricao" => "Um curso de  Mobile incrivel","preco" =>1500,"img"=> "img/produto2.jpg"],
-"produto3" => ["nome"=> "Curso Fullstack","descricao" => "Um curso de Programação incrivel" ,"preco" =>1200,"img" => "img/produto1.jpeg"],
-"produto4" => ["nome"=> "Curso Mobile Android","descricao" => "Um curso de  Mobile incrivel","preco" =>1500,"img"=> "img/produto2.jpg"],
-"produto5" => ["nome"=> "Curso Fullstack","descricao" => "Um curso de Programação incrivel" ,"preco" =>1200,"img" => "img/produto1.jpeg"],
-"produto6" => ["nome"=> "Curso Mobile Android","descricao" => "Um curso de  Mobile incrivel","preco" =>1500,"img"=> "img/produto2.jpg"],
-];
+<!DOCTYPE html>
+<html lang="en">
 
-$categorias = ["Cursos","Tutoriais","Artigos","Forum","Códigos"];
-    ?>
+<?php include "head.php"; ?>
 
+<body>
+    <?php include_once "header.php"; ?>
+    <?php include_once "header.php"; ?>
+    <main class="container mt-5 ">
+        <section class="row">
+        <!-- Coluna para segurar card -->
 
-   <!DOCTYPE html>
-   <html lang="en">
-
-   <head>
-     <meta charset="UTF-8" />
-     <meta name="viewport" content="width=device-width, initial-scale=1">
-     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-     <title>Loja Virtual</title>
-     <!-- Bootstrap -->
-     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-       integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-   </head>
-
-   <body>
-
-
-     <header>
-       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-         <a class="navbar-brand" href="#">Cursos</a>
-         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-           aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-           <span class="navbar-toggler-icon"></span>
-         </button>
-         <div class="collapse navbar-collapse" id="navbarNav">
-           <ul class="navbar-nav">
-
-             <?php if(isset($usuario) && $usuario != "" && $usuario["logado"]): ?>
-
-             <?php if($usuario['nivelAcesso'] == 0): ?>
-             <li class="nav-item active">
-               <a class="nav-link" href="#">Ações<span class="sr-only">(current)</span></a>
-             </li>
-             <?php  else: ?>
-             <li class="nav-item active">
-               <a class="nav-link" href="#">Perfil <span class="sr-only">(current)</span></a>
-             </li>
-             <?php endif; ?>
-             <li class="nav-item">
-               <a class="nav-link" href="#"> Olá <?php echo $usuario['nome']; ?></a>
-             </li>
-             <?php else: ?>
-
-             <li class="nav-item">
-               <a class="nav-link" href="#"> Login </a>
-             </li>
-             <?php endif; ?>
-           </ul>
-         </div>
-       </nav>
-       <nav>
-         <ul class="row mt-3 justify-content-center">
-           <?php foreach ($categorias as $categoria): ?>
-           <li class="col-md-2"><?=$categoria?></li>
-           <?php endforeach; ?>
-         </ul>
-       </nav>
-     </header>
+            <?php foreach($produtos as $chave=>$produto): ?>
+            <div class="col-md-4 mt-3">
+                <div class="card" style="width: 18rem;">
+                    <img src="<?php echo $produto["img"]; ?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $produto["nome"]; ?></h5>
+                        <p class="card-text"><?php echo $produto["descricao"]; ?></p>
+                        <h4 class="text-success">R$<?php echo $produto["preco"]; ?></h4>
+                            <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#<?php echo $produto["id"]; ?>">
+                            Comprar
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <?php endforeach; ?> 
+        </section>
+    </main>
 
 
-     <main class="container mt-5">
-       <section class="row">
-         <!-- Coluna para segurar card -->
-         <?php foreach($produtos as $chave=> $value) {
+<!-- Modal -->
 
-?>
-         <div class="col-md-4 mt-3">
-           <div class="card" style="width: 18rem;">
-             <img src="<?php echo $value ["img"];?>" class="card-img-top" alt="...">
-             <div class="card-body">
-               <h5 class="card-title"><?php echo $value ["nome"];?></h5>
-               <p class="card-text"><?php echo $value ["descricao"]; ?></p>
-               <h4 class="text-success">R$<?php echo $value ["preco"]; ?></a>
-                 <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#<?php echo $chave; ?>">
-                   Comprar
-                 </button>
-             </div>
-           </div>
-         </div>
-         <?php } ?>
-       </section>
-     </main>
-     <!-- Button trigger modal -->
+<?php foreach($produtos as $chave=>$produto): ?>
+<div class="modal fade" id="<?php echo $produto["id"]; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Produto: <?php echo $produto["nome"]; ?></h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form method="POST" action="sucesso.php">
+            <div class="form-group">
+                <input type="text" name="nomeProduto" value="<?php echo $produto["nome"]; ?>" hidden>
+                <input type="text" name="nomeCliente" placeholder="Nome completo">
+            </div>
+            <div class="form-group">
+                <input type="number" name="cpfCliente" placeholder="CPF">
+            </div>
+             <div class="form-group">
+                <input type="number" name="cartaoCliente" placeholder="Cartão de crédito">
+            </div>
+             <div class="form-group">
+                <input type="date" name="dataValidadeCartao" placeholder="Data de validade do cartão">
+            </div>
+             <div class="form-group">
+                <input type="number" maxlength="3" name="cvvCartao" placeholder="CVV do cartão">
+            </div>
+        
+      </div>
+      <div class="modal-footer">
+        <div class="text-primary">Preço total: R$ <?php echo $produto["preco"]; ?></div>
+        <button type="submit" class="btn btn-success">Finalizar Compra</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+<?php endforeach; ?>
 
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+</body>
 
-     <?php foreach($produtos as $chave => $produto): ?>
-     <!-- Modal -->
-     <div class="modal fade" id="<?php echo $chave ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-       aria-hidden="true">
-       <div class="modal-dialog" role="document">
-         <div class="modal-content">
-           <div class="modal-header">
-             <h5 class="modal-title" id="exampleModalLabel">Produto: <?php echo $produto ["nome"]; ?></h5>
-             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-               <span aria-hidden="true">&times;</span>
-             </button>
-           </div>
-           <div class="modal-body">
-             <form>
-               <div class="form-group">
-                 <input type="text" name="nomeCliente" placeholder="Nome Completo">
-               </div>
-               <div class="form-group">
-                 <input type="number" name="cpfCliente" placeholder="CPF">
-               </div>
-               <div class="form-group">
-                 <input type="number" name="cartaoCliente" placeholder="Cartão de crédito">
-               </div>
-               <div class="form-group">
-                 <input type="text" name="dataDeValidade" placeholder="Data de validade">
-               </div>
-               <div class="form-group">
-                 <input type="number" maxlenght="3" name="cpfCliente" placeholder="CVV do cartão">
-               </div>
-               <div class="form-group">
-                 <input type="number" name="cpfCliente" placeholder="CPF">
-               </div>
-             </form>
-           </div>
-           <div class="modal-footer">
-             <div class="text-primary">Preço total : R$ <?php echo  $produto ['preco']; ?></button>
-               <button type="button" class="btn btn-success">Finalizar Compra </button>
-             </div>
-           </div>
-         </div>
-       </div>
-     </div>
-     <?php endforeach; ?>
-
-
-     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-       integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
-     </script>
-     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-       integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
-     </script>
-     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-       integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
-     </script>
-   </body>
-
-   </html>
+</html>
